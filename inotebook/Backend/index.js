9ed7ca685542c8941f84cfd58ff0d076b242080d
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-02 19:01:35 +0530

    Modifying index.js

--- inotebook/Backend/index.js
+++ inotebook/Backend/index.js
@@ -10,9 +10,8 @@ const app = express()
 const port = 3000
 
 // Availables Routes
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
