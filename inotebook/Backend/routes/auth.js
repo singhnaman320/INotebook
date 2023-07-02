6eca12f806ee8694972af09769837207125f062b
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-02 19:16:59 +0530

    Modifying routes.js

--- inotebook/Backend/routes/auth.js
+++ inotebook/Backend/routes/auth.js
@@ -1,3 +1,14 @@
 const express = require('express') // import
-
 const router = express.Router();
+
+router.get('/', (req, res) =>{
+
+    obj = {
+        a: 'naman',
+        number: 55
+    }
+
+    res.json(obj);
+})
+
+module.exports = router
