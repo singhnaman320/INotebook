c4d589e1e531bf0440365bbdc3d8a0365470f805
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-02 14:14:07 +0530

    Adding mongoose

--- inotebook/Backend/db.js
+++ inotebook/Backend/db.js
@@ -4,6 +4,8 @@ const mongoURI = 'mongodb://localhost:27017/?tls=false&directConnection=true'
 const connectToMongo = () =>{
 
     mongoose.connect(mongoURI, () => {
-        
+
+        console.log('connected to Mongo successfully..!')
     })
 }
+
