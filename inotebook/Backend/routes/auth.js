79d6b8d9abfac159d5e916a999d7f5a1c11bf404
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-03 12:10:23 +0530

    Modifying index.js

--- inotebook/Backend/routes/auth.js
+++ inotebook/Backend/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express') // import
 const router = express.Router();
 
+// Create a user uning POST: "/api/auth/". doesn't require auth
+
 router.get('/', (req, res) =>{
 
     console.log(req.body);
