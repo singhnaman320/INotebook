a006218fedfeb6c5acac4214e473388d5c3cf82c
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-03 12:03:47 +0530

    Modifying auth.js

--- inotebook/Backend/index.js
+++ inotebook/Backend/index.js
@@ -6,8 +6,6 @@ connectToMongo();
 
 // From : [https://expressjs.com/en/starter/hello-world.html]
 const express = require('express')
-const app = express()
-const port = 3000
 
 // Availables Routes
 app.use('/api/auth', require('./routes/auth'))
