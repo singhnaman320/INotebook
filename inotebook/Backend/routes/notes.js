03c84e3568e879e90293b3cec786c33af07d78f4
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-04 20:03:24 +0530

    Adding notes

--- inotebook/Backend/routes/notes.js
+++ inotebook/Backend/routes/notes.js
@@ -2,6 +2,7 @@ const express = require('express') // import
 const router = express.Router();
 const fetchUser = require('../middleware/fetchUser')
 const Notes = require('../models/Notes')
+const { body, validationResult } = require('express-validator');
 
 // ROUTE-1: Fetch all the notes using GET: "/api/auth/fetchallnotes". Login required
 router.get('/fetchallnotes', fetchUser, async (req, res) =>{
