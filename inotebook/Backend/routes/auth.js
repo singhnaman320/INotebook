4428819edaf03f36e9162529b31a11c10b7d76b4
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-04 11:54:59 +0530

    Adding bcrypt.js

--- inotebook/Backend/routes/auth.js
+++ inotebook/Backend/routes/auth.js
@@ -26,13 +26,13 @@ router.post('/createUser', [
             return res.status(400).json({error: "sorry.!, a user with this email already exists"})
         }
         // Bcrypting Password
-        let salt = bcrypt.genSalt(10);
-        securedPassword = bcrypt.hash(req.body.password, salt)
+        const salt = await bcrypt.genSalt(10);
+        const securedPasswordHash = await bcrypt.hash(req.body.password, salt)
         user = await User.create({
 
             name : req.body.name,
             email : req.body.email,
-            password : securedPassword
+            password : securedPasswordHash
 
         })
         res.json(user)
