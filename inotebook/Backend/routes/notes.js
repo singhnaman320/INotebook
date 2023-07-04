1778bc45874eaecf640bb8f32dfa257c2a19ba9e
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-04 18:43:31 +0530

    Fetching all notes

--- inotebook/Backend/routes/notes.js
+++ inotebook/Backend/routes/notes.js
@@ -1,9 +1,12 @@
 const express = require('express') // import
-const notes = express.Router();
+const router = express.Router();
+const fetchUser = require('../middleware/fetchUser')
+const Notes = require('../models/Notes')
 
+// ROUTE-1: Fetch all the notes using GET: "/api/auth/fetchallnotes". Login required
 notes.get('/fetchallnotes', (req, res) =>{
 
     res.json([]);
 })
 
-module.exports = notes
+module.exports = router
