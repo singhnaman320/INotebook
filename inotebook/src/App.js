d9b281373abd440c3bd866c41efc617af83c3289
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-05 18:13:26 +0530

    Adding Routes to App.js

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -1,13 +1,22 @@
 // import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import Home from './components/Home';
+import{BrowserRouter as Router, Switch, Route, Link}from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <h1>INotebook - Your personal notebook</h1>
-    </div>
+    <>
+      <Router>
+        <NavBar/>
+        <h1>INotebook - Your personal notebook</h1>
+        <Home/>
+        <Switch>
+          <Route></Route>
+        </Switch>
+      </Router>
+      
+    </>
   );
 }
 
