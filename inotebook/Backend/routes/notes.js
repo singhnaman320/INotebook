018bc4a979e6050fed5ce8e64bcdbbe6e93dde83
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-05 11:45:00 +0530

    Update existing note

--- inotebook/Backend/routes/notes.js
+++ inotebook/Backend/routes/notes.js
@@ -53,6 +53,37 @@ router.post('/addnote', fetchUser, [
 })
 
 // ROUTE-3: Update a existing Note using POST: "/api/auth/updatenote". Login required (Token must also be sent with he headers)
+router.post('/updatenote', fetchUser, [
+    // Validations are to be written here in routes
+    body('title', 'Enter a valid title.!').isLength({min : 3}),
+    body('description', 'Description must be at least of 10 character.!').isLength({min : 5})
+    ], async (req, res) =>{
+
+    try {
+        
+        const{title, description, tag} = req.body;   
+    
+        // if there are errors, return bad request and the errors
+        const result = validationResult(req)
+        if(!result.isEmpty()){
+            return res.status(400).json({errors: result.array()});
+        }       
+
+        // Creating new note
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        })
+
+        const savedNotes = await note.save();
+
+        res.json(savedNotes);
+
+    } catch (error) {
+        
+        console.log(error.message)
+        res.status(500).send("Some error occured..!");
+    } 
+})
 
 
 module.exports = router
