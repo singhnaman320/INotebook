753993c0a000a7fcba1c766fa899b72b72e20f89
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-06 13:30:22 +0530

    Adding NoteState to App.js

--- inotebook/src/components/About.js
+++ inotebook/src/components/About.js
@@ -6,6 +6,13 @@ const About = () => {
 
   const usage = useContext(noteContext);
 
+  useEffect(() => {
+    
+    usage.update();
+
+  }, [])
+  
+
   return (
     <div>
       This is About {usage.name} and he is persuing {usage.education}
