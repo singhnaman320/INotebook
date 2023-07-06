e468f232bd19ca3e82706cf45b1af074e64e7238
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-06 14:26:04 +0530

    Adding NoteState to App.js

--- inotebook/src/context/notes/NoteState.js
+++ inotebook/src/context/notes/NoteState.js
@@ -1,34 +1,10 @@
 import noteContext from './noteContext'
-import { useState } from 'react'
 
 const NoteState = (props) => {
 
-    const state1 = {
-
-        "name" : "Naman",
-        "education" : "Engineering"
-
-    }
-
-    const [state, setState] = useState(state1)
-
-    const update = () => {
-
-        setTimeout(() => {
-
-            setState({
-
-                "name" : "Kaushik",
-                "education" : "CS Engineering"
-
-            })
-            
-        }, 2000);
-    }
-
     return(
 
-        <noteContext.Provider value = {{state, update}}>
+        <noteContext.Provider value = {{}}>
             {props.children}
         </noteContext.Provider>
     )
