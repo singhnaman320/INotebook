e1a581701431eb52210e3b18ed64beba20b14945
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-06 14:28:23 +0530

    Adding NoteState to App.js

--- inotebook/src/components/About.js
+++ inotebook/src/components/About.js
@@ -1,10 +1,7 @@
 import React, {useContext} from 'react'
-import noteContext from '../context/notes/noteContext'
 
 // Using context in About
 const About = () => {
-
-  const usage = useContext(noteContext);
 
   return (
     <div>
