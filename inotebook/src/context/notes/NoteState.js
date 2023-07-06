91dd43c1d8e61d2e13bb8c5023963d909dab6a08
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-06 12:22:38 +0530

    Adding NoteState

--- inotebook/src/context/notes/NoteState.js
+++ inotebook/src/context/notes/NoteState.js
@@ -1,8 +1,14 @@
 import React from "react";
 import noteContext from './noteContext'
 
-const NoteState = () => {
+const NoteState = (props) => {
 
+    return(
+
+        <NoteState.provider>
+            props.children;
+        </NoteState.provider>
+    )
 }
 
 export default NoteState;
