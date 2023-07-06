b1476769ada606f0e7ebe493d82e9cf9b9cc383b
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-06 12:26:45 +0530

    Adding NoteState

--- inotebook/src/context/notes/NoteState.js
+++ inotebook/src/context/notes/NoteState.js
@@ -3,9 +3,16 @@ import noteContext from './noteContext'
 
 const NoteState = (props) => {
 
+    const state = {
+
+        "name" : "Naman",
+        "Education" : "Engineering"
+
+    }
+
     return(
 
-        <NoteState.provider>
+        <NoteState.provider value = {state}>
             {props.children}
         </NoteState.provider>
     )
