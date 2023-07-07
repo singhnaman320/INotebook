0c5c4916cf2dfadb5870b41481371804ecd08850
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-07 14:56:10 +0530

    Adding AddNote componenet to Home.js

--- inotebook/src/components/Home.js
+++ inotebook/src/components/Home.js
@@ -1,11 +1,12 @@
 // rface -> react arrow functional component with export
+import AddNote from "./AddNote";
 import Notes from "./Notes";
 
 const Home = () => {
 
   return (
     <div>
-      
+      <AddNote/>
       <Notes/>
     </div>
   )
