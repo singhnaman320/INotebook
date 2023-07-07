ab022733a588ed8f7f578fba0aaf2d178a79335c
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-07 15:10:07 +0530

    Modifying AddNote.js

--- inotebook/src/components/AddNote.js
+++ inotebook/src/components/AddNote.js
@@ -18,12 +18,12 @@ const AddNote = () => {
             <form className='my-3'>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={onChange}/>
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    <input type="password" className="form-control" id="exampleInputPassword1"  onChange={onChange}/>
                 </div>
                 <div className="mb-3 form-check">
                     <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
