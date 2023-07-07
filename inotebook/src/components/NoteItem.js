08766ee2628ade92b4e5d2f239a703334820db57
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-07 12:29:11 +0530

    Modifying NoteItem.js

--- inotebook/src/components/NoteItem.js
+++ inotebook/src/components/NoteItem.js
@@ -7,10 +7,12 @@ const NoteItem = (props) => {
     <div className='col-md-3'>
         <div className="card my-3">
             <div className="card-body">
+              <div className='d-flex align-items-center'>
                 <h5 className="card-title">{note.title}</h5>
-                <p className="card-text">{note.description}</p>
-                <i class="fa-sharp fa-solid fa-trash"></i> {/* For delete icon */}
-                <i class="fa-solid fa-pen-to-square mx-2"></i> {/* For edit icon from*/}
+                <i className="fa-sharp fa-solid fa-trash mx-2"></i> {/* For delete icon */}
+                <i className="fa-solid fa-pen-to-square mx-2"></i> {/* For edit icon from*/}
+              </div>
+              <p className="card-text">{note.description}</p>
             </div>
         </div>
     </div>
