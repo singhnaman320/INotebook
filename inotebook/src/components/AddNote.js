b8eaaa0718e4e4aecb51d6326227042b5a1a0fae
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-07 15:08:04 +0530

    Modifying AddNote.js

--- inotebook/src/components/AddNote.js
+++ inotebook/src/components/AddNote.js
@@ -3,32 +3,36 @@ import noteContext from '../context/notes/noteContext';
 
 const AddNote = () => {
 
-  const context = useContext(noteContext);
-  const{addNote} = context;
+    const context = useContext(noteContext);
+    const{addNote} = context;
 
-  return (
+    const handleClick = () => {
+
+    }
+
+    return (
     
-    <div className="container my-3">
-        <h2>|| Add your note here ||</h2>
+        <div className="container my-3">
+            <h2>|| Add your note here ||</h2>
 
-        <form className='my-3'>
-          <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1"/>
-          </div>
-          <div className="mb-3 form-check">
-            <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-          </div>
-          <button type="submit" className="btn btn-primary" onClick={handleClick}>Submit</button>
-        </form>
-    </div>
-  )
+            <form className='my-3'>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                </div>
+                <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" className="btn btn-primary" onClick={handleClick}>Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default AddNote
