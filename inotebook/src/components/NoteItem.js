d1ed6b953b1dec521988017d7d0194f7a13670ab
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-07 16:11:43 +0530

    Adding deleteNote in NoteItem.js

--- inotebook/src/components/NoteItem.js
+++ inotebook/src/components/NoteItem.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import { React, useContext, useState } from 'react'
+import noteContext from '../context/notes/noteContext';
 
 const NoteItem = (props) => {
 
+  const context = useContext(noteContext);
+  const {deleteNote} = context; 
+
   const {note} = props;  
+
   return (
     <div className='col-md-3'>
         <div className="card my-3">
             <div className="card-body">
               <div className='d-flex align-items-center'>
                 <h5 className="card-title">{note.title}</h5>
-                <i className="fa-sharp fa-solid fa-trash mx-2"></i> {/* For delete icon */}
+                <i className="fa-sharp fa-solid fa-trash mx-2" onClick={() => {deleteNote(note._id)}}></i> {/* For delete icon */}
                 <i className="fa-solid fa-pen-to-square mx-2"></i> {/* For edit icon from*/}
               </div>
               <p className="card-text">{note.description}</p>
