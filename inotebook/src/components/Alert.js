51f6ab862b7b19c2e685ea8e2906bb9096d4bd78
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-07 12:47:09 +0530

    Adding Alert.js

--- inotebook/src/components/Alert.js
+++ inotebook/src/components/Alert.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Alert = () => {
   return (
     <div>
-      
+        <div class="alert alert-primary" role="alert">
+            A simple primary alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
+        </div>
     </div>
-  )
-}
+  );
+};
