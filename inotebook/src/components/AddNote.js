ce5155184735c44a5d13ec042610c1f10b5b5b8c
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-08 12:11:37 +0530

    Modifying AddNote component

--- inotebook/src/components/AddNote.js
+++ inotebook/src/components/AddNote.js
@@ -35,6 +35,10 @@ const AddNote = () => {
                     <label htmlFor="description" className="form-label">Description</label>
                     <input type="text" className="form-control" id="description"  name="description" onChange={onChange}/>
                 </div>
+                <div className="mb-3">
+                    <label htmlFor="tag" className="form-label">Tag</label>
+                    <input type="text" className="form-control" id="tag"  name="tag" onChange={onChange}/>
+                </div>
                 <button type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button>
             </form>
         </div>
