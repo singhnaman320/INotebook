72e0e47fe5ea4dcd17407fe8970b7fa3af8eb8bd
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-08 16:29:51 +0530

    Modifying Note.js for edit

--- inotebook/src/components/NavBar.js
+++ inotebook/src/components/NavBar.js
@@ -15,18 +15,14 @@ const NavBar = () =>{
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to={'/'}>Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to={'/about'}>About</Link>
-                    </li>
-                </ul>
-                <form className="d-flex" role="search">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to={'/'}>Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to={'/about'}>About</Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
