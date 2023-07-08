090c5b47d3e46871ad7144581c4073a196813aa6
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-08 12:28:41 +0530

    Modifying Note.js for edit

--- inotebook/src/components/Notes.js
+++ inotebook/src/components/Notes.js
@@ -15,15 +15,43 @@ const Notes = () => {
 
   }, [])
   
+  const updateNote = (note) => {
+
+
+  }
 
   return (
     <>
       <AddNote/>
+      {/* Button trigger modal */}
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
+
+      {/* Modal */}
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="container row my-3">
           <h2>Your Notes</h2>
           {notes.map((note) =>{
 
-            return <NoteItem key = {note._id} note = {note}/>  // _id come from Mongodb
+            return <NoteItem key={note._id} updateNote={updateNote} note={note}/>  // _id come from Mongodb
 
           })}
       </div>
