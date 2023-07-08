bfcbe64bb3983632eafc51fa6496b19edffc22b0
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-08 15:19:02 +0530

    Modifying Note.js for edit

--- inotebook/src/components/Notes.js
+++ inotebook/src/components/Notes.js
@@ -16,18 +16,20 @@ const Notes = () => {
   }, [])
   
   const ref = useRef(null)
-  const [note, setNote] = useState({etitle: "", edescription:"", etag:""})
+  const refClose = useRef(null)
+
+  const [note, setNote] = useState({id: "", etitle: "", edescription:"", etag:""})
 
   const updateNote = (currentNote) => {
 
     ref.current.click();
-    setNote({etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag})
+    setNote({id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag})
   }
 
   const handleClick = (e) => {
 
     console.log("Updating the note: ", note)
-    e.preventDefault();
+    refClose.current.click();
   }
 
   const onChange = (event) =>{
@@ -71,7 +73,7 @@ const Notes = () => {
               </form>
             </div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button  ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
               <button onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
             </div>
           </div>
