e1e6b2be724bd8c24cbde0a7d0174b8ad8a1c8d4
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-09 20:17:25 +0530

    Modifying Nvbar.js for Logout

--- inotebook/src/components/Login.js
+++ inotebook/src/components/Login.js
@@ -44,7 +44,8 @@ const Login = (props) => {
   }
 
   return (
-    <div>
+    <div className='mt-3'>
+        <h2>Login to continue to INotebook</h2>
         <form  onSubmit={handleSubmit}>  {/* On submit will be form not button */}
             <div className="mb-3">
                 <label htmlFor="email" className="form-label">Email address</label>
