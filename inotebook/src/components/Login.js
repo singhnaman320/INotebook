2b77ef250cd25321735f82940e6a3c1f9d80b323
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-09 12:48:24 +0530

    Adding Login form in Login.js

--- inotebook/src/components/Login.js
+++ inotebook/src/components/Login.js
@@ -2,8 +2,7 @@ import React, { useState } from 'react'
 
 const Login = () => {
 
-  const [email, setEmail] = useState("")  
-  const [password, setPassword] = useState("")  
+  const [credentials, setCredentials] = useState({email: "", password: ""})  
 
   const handleSubmit = async(e) =>{
 
