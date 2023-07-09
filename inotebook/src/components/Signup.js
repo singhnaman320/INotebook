e6e9921f80bead6209a93100a7518cacd256d63b
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-09 14:21:40 +0530

    Adding Signup.js

--- inotebook/src/components/Signup.js
+++ inotebook/src/components/Signup.js
@@ -1,25 +1,33 @@
 import React from 'react'
 
 const Signup = () => {
+
+  const onChange = (event) =>{
+    // ... -> spread operator
+    
+    // means all the values which is inside this note object will be there but the properties written after that must be overwritten 
+    setCredentials({...credentials, [event.target.name]: event.target.value});
+  }  
+  
   return (
     <div className='container'>
         <form>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input type="text" className="form-control" id="name" aria-describedby="emailHelp"/>
+                <input type="text" className="form-control" id="name" onChange={onChange} aria-describedby="emailHelp"/>
             </div>
             <div className="mb-3">
                 <label htmlFor="email" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="email" aria-describedby="emailHelp"/>
+                <input type="email" className="form-control" id="email" onChange={onChange} aria-describedby="emailHelp"/>
                 <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div className="mb-3">
                 <label htmlFor="password" className="form-label">Password</label>
-                <input type="password" className="form-control" id="password"/>
+                <input type="password" className="form-control" id="password" onChange={onChange}/>
             </div>
             <div className="mb-3">
                 <label htmlFor="cpassword" className="form-label">Confirm Password</label>
-                <input type="cpassword" className="form-control" id="cpassword"/>
+                <input type="cpassword" className="form-control" id="cpassword" onChange={onChange}/>
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
