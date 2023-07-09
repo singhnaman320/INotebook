d886bd759e063fae875414b4248b8ffc0a8613c6
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-09 12:45:51 +0530

    Adding Login form in Login.js

--- inotebook/src/components/Login.js
+++ inotebook/src/components/Login.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [email, setEmail] = useState("")  
+  const [password, setPassword] = useState("")  
 
   const handleSubmit = async(e) =>{
 
@@ -32,12 +35,12 @@ const Login = () => {
         <form  onSubmit={handleSubmit}>  {/* On submit will be form not button */}
             <div className="mb-3">
                 <label htmlFor="email" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp"/>
+                <input type="email" className="form-control" id="email" name="email" value={email} aria-describedby="emailHelp"/>
                 <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div className="mb-3">
                 <label htmlFor="password" className="form-label">Password</label>
-                <input type="password" className="form-control" id="password" name="password"/>
+                <input type="password" className="form-control" id="password" name="password" value={password}/>
             </div>
             <button type="submit" className="btn btn-warning">Submit</button>
         </form>
