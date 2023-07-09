5bb73f243f42a6e37a58e345ff280713b68ac608
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-09 14:16:00 +0530

    Adding Signup.js

--- inotebook/src/components/Signup.js
+++ inotebook/src/components/Signup.js
@@ -5,13 +5,21 @@ const Signup = () => {
     <div className='container'>
         <form>
             <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                <label htmlFor="name" className="form-label">Name</label>
+                <input type="text" className="form-control" id="name" aria-describedby="emailHelp"/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="email" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="email" aria-describedby="emailHelp"/>
                 <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1"/>
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password" className="form-control" id="password"/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="cpassword" className="form-label">Confirm Password</label>
+                <input type="cpassword" className="form-control" id="cpassword"/>
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
